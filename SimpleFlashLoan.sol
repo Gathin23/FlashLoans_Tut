5e33450d02f22aaef3b8ca0d08c1489898a8e7ec
Gathin23 <gathint23@gmail.com>
2023-11-27 18:42:02 +0530

    Flash Loan

--- SimpleFlashLoan.sol
+++ SimpleFlashLoan.sol
@@ -1,3 +1,66 @@
 //SPDX-Liscense-Identifier: MIT
 
-pragma solidity 0.8.20;
+pragma solidity 0.8.10;
+
+
+import "https://github.com/aave/aave-v3-core/blob/master/contracts/flashloan/base/FlashLoanSimpleReceiverBase.sol";
+import "https://github.com/aave/aave-v3-core/blob/master/contracts/interfaces/IPoolAddressesProvider.sol";
+import "https://github.com/aave/aave-v3-core/blob/master/contracts/dependencies/openzeppelin/contracts/IERC20.sol";
+
+contract SimpleFlashLoan is FlashLoanSimpleReceiverBase {
+    address payable owner;
+
+    constructor(address _addressProvider)
+        FlashLoanSimpleReceiverBase(IPoolAddressesProvider(_addressProvider))
+    {
+    }
+
+    function fn_RequestFlashLoan(address _token, uint256 _amount) public {
+        address receiverAddress = address(this);
+        address asset = _token;
+        uint256 amount = _amount;
+        bytes memory params = "";
+        uint16 referralCode = 0;
+
+        //Requesting from the pool
+
+        POOL.flashLoanSimple(
+            receiverAddress,
+            asset,
+            amount,
+            params,
+            referralCode
+        );
+
+    }
+    /**
+        This function is called after your contract has received the flash loaned amount
+     */
+    function  executeOperation(
+        address asset,
+        uint256 amount,
+        uint256 premium,
+        address initiator,
+        bytes calldata params
+    )  external override returns (bool) {
+        //
+        // This contract now has the funds requested.
+        // Your logic goes here.
+        //
+
+        // At the end of your logic above, this contract owes
+        // the flashloaned amount + premiums.
+        // Therefore ensure your contract has enough to repay
+        // these amounts.
+
+        // Approve the Pool contract allowance to *pull* the owed amount
+        uint256 totalAmount = amount + premium;
+        IERC20(asset).approve(address(POOL), totalAmount);
+
+        return true;
+    }
+
+    receive() external payable {}
+
+}
+
